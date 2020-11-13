5c534a5671b34e32d92a932ab0ce37c0e8245a6e
Firdiansyah R <firdi.ansyah20@gmail.com>
2020-11-13 17:02:41 +0700

    Stable Version TracingGame

---
+++ Assets/DemoTracingGame/TracingSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracingSceneManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        TracingGame.InstantiateGameOnScene(GameWords.Fa , ()=> { Debug.Log("This is CallBack"); });
+    }
+}
